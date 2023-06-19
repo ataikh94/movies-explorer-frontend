9b4e249c115c48cc573e9eb53b1bf9c8f322fca6
Anna Taikh <kozina.aniuta2014@yandex.ru>
2023-06-19 19:02:14 +0300

    Fix: изменения в блоке cardlist-изменен тип блока

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -4,10 +4,10 @@ import MoviesCard from '../MoviesCard/MoviesCard';
 
 export default function MoviesCardList() {
   return (
-    <section className='movies-card-list'>
+    <div className='movies-card-list'>
       <span className='movies-card-list__no-data'>Ничего не найдено</span>
       <MoviesCard />
       <button type='button' className='movies-card-list__button_else'>Ещё</button>
-    </section>
+    </div>
   )
 }
